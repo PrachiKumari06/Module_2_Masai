2d53641efa86b396a7f03cb17c0883347060f63d
Prachi Kumari <prachi.kumari0004@gmail.com>
2025-11-10 01:36:04 +0530

    Added Basic_Programming project inside Module_2_Masai

---
+++ Basic_Programming/index.js
@@ -0,0 +1,19 @@
+function isPrime(num) {
+  if (num <= 1) return false;
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) return false;
+  }
+  return true;
+}
+
+console.log(isPrime(5)); 
+console.log(isPrime(8)); 
+
+
+
+function isEven(num) {
+  return num % 2 === 0;
+}
+
+console.log(isEven(4)); 
+console.log(isEven(7)); 
